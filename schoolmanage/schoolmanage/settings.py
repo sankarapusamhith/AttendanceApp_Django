b35337a6a7f7b5b143df5ae01ea0c94ff174a25a
samhith <sankarapusamhith1999@gmail.com>
2020-12-23 08:05:44 +0530

    attendancereg

--- schoolmanage/schoolmanage/settings.py
+++ schoolmanage/schoolmanage/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import Django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,5 +122,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-django_heroku.settings(locals())
 
